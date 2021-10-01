b724f341af65fe39ccacaf50c3523d437f7d8e4e
Richard Crosby <crozuk@gmail.com>
2021-10-01 14:09:42 +0100

    post session with me@ub

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var secret_key = '640c4a5621c055a20267eebd06495463dc02c040be328945';
+
 //Express server
 var express = require('express');
 var app = express();
 const port = 3001;
+
+var parse = require('body-parser');
+var fetch = require('node-fetch');
 
 //Static file path
 app.use(express.static('static'));
@@ -10,8 +15,46 @@ app.use(express.static('static'));
 app.get('/', function(req, res){
   res.sendFile('static/index.html');
 });
-
+app.post('/search', parse.urlencoded(), async function(req, res) {
+  console.log("body is", req.body);
+  var search_term = req.body.search_term;
+  console.log(search_term);
+  var response = await fetch("https://api.automationcloud.net/jobs", {
+    method: "post",
+    headers: {
+      authorization: "Basic " + Buffer.from(secret_key + ":").toString("base64")
+    },
+    body: JSON.stringify({
+      serviceId: "20ea0e52-1c0d-41ba-9ed2-4b50ca847f31",
+      category: "test",
+      input: {
+        search_term
+      }
+    })
+  })
+  var body = await response.json();
+  var results = await pollJobOutput(body.id, "SearchResults");
+  res.send(results);
+});
 //Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
+
+async function pollJobOutput(jobId, outputKey){
+  var response = await fetch("https://api.automationcloud.net/jobs/" + jobId + "/outputs/" + outputKey, {
+    headers: {
+      authorization: "Basic " + Buffer.from(secret_key + ":").toString("base64")
+    }
+  });
+  var body = await response.json();
+  if (body.code === "notFound"){
+    await timeout(1000);
+    return pollJobOutput(jobId, outputKey)
+  }
+  return body;
+};
+
+function timeout(ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
